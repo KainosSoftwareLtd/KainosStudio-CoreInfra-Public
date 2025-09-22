10683b63799e9e70b5ded82a99fc942a9795afc9
Nick Moynihan <nick.moynihan@kainos.com>
2025-09-22 12:42:11 +0100

    Fix name os kainoscore-static-files name in locals.tf

--- aws/prod-infra/prod/locals.tf
+++ aws/prod-infra/prod/locals.tf
@@ -2,7 +2,7 @@ locals {
   cloudfront_comment                     = "Core Distribution ${var.env}"
   s3_bucket_kfd_name                     = "kainoscore-kfdfiles-${var.env}"
   s3_bucket_zip_files_name               = "kainoscore-zip-files-${var.env}"
-  s3_bucket_static_files_name            = "kainoscore-staticfiles-${var.env}"
+  s3_bucket_static_files_name            = "kainoscore-static-files-${var.env}"
   s3_bucket_submitted_form_data_name     = "kainoscore-submitted-form-data-${var.env}"
   s3_bucket_core_audit_logs_name         = "kainoscore-audit-logs-${var.env}"
   dynamodb_core_form_sessions_table_name = "Core_FormSessions_${var.env}"
