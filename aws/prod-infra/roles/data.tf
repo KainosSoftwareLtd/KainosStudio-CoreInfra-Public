e4634bb12250eb487729552021a331d039afe830
Nick Moynihan <nick.moynihan@kainos.com>
2025-09-25 14:54:14 +0100

    alter codestar

--- aws/prod-infra/roles/data.tf
+++ aws/prod-infra/roles/data.tf
@@ -3,5 +3,5 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_codestarconnections_connection" "kainosstudio" {
-  name = "KainosStudio"
+  name = "KainosStudioProd"
 }
